39bdea078d3345f82f7563eaedeb233eec3fa147
Numan Laanait <nlaanait@gmail.com>
2024-01-28 11:12:29 -0500

    add license and copyright

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+/*
+ Copyright (c) 2024 Fairomics LLC
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use ngrok::prelude::*;
 use patient_access_api::{app, handlers, settings::load_settings};
 use std::error::Error;
